b039e632dedb898ef36ec3010d5e682080389133
Carlos <carlosdiones28@gmail.com>
2022-05-11 14:26:31 -0300

    removes unnecessary components

--- learning-react/src/components/Header/NavigationControl/index.js
+++
@@ -1,9 +0,0 @@
-import './index.css';
-
-export function NavigationControl(){
-    return(
-        <div className="nav-container">
-
-        </div>
-    );
-}
--- learning-react/src/components/Header/NavigationTop/SiteLogo/index.js
+++
@@ -1,7 +0,0 @@
-import './index.css';
-
-export function SiteLogo(props){
-    return (
-       <img className="logo-image" src={props.source} alt={props.title}></img>
-    );
-}
